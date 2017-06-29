d8f964e0d989b848eb31f48f0171ad327ab01927
yikai <yikai8053@gmail.com>
2017-06-29 17:58:52 +0800

    eng

--- Linebot/urls.py
+++ Linebot/urls.py
@@ -13,16 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 from Linebot.views import here
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^here/$', here),
-    
-]
-
 
 import echobot
 
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^here/$', here),
+    url(r'^echobot/', include('echobot.urls')),
+]
