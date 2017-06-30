3ee20a0a24fc4db0b5571c87865e4075653c3ef4
yikai <yikai8053@gmail.com>
2017-06-30 10:38:58 +0800

    eng

--- echobot/views.py
+++ echobot/views.py
@@ -33,7 +33,8 @@ def callback(request):
                 if isinstance(event.message, TextMessage):
                     line_bot_api.reply_message(
                         event.reply_token,
-                        TextSendMessage(text=event.message.text)
+                        # TextSendMessage(text=event.message.text)
+                        TextSendMessage(text='test')
                     )
 
         return HttpResponse()
